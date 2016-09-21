e184fce24fef52939bc38e81bcad9048823afd73
Tony  Merante <merante@gmail.com>
2016-09-21 14:41:36 -0500

    After Day One

---
+++ WeatherSky/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WeatherSky
+//
+//  Created by Tony Merante on 9/20/16.
+//  Copyright © 2016 Tony Merante. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
